485954226f76cf32afb1e1f0e9bba5e5f42bd18e
Nat <natalieb007@googlemail.com>
2023-09-05 17:27:32 +0100

    create SelectRule component

---
+++ Cryptid/Components/Create/SelectRule.razor.cs
@@ -0,0 +1,26 @@
+﻿using Cryptid.Models.Enums;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cryptid.Components.Create
+{
+    public partial class SelectRule
+    {
+        [Parameter]
+        public Player Player { get; set; }
+
+        private Game GameToCreate { get; set; } = new Game();
+
+
+        public void HandleRuleNumberChanged(int ruleNumber)
+        {
+            Console.Out.WriteLine(ruleNumber);
+        }
+
+        private class Game
+        {
+            [Required, Range(1, 96, ErrorMessage = "Rule Number must be between 1 and 96")]
+            public int RuleNumber { get; set; }
+        }
+    }
+}
--- Cryptid/Pages/Create.razor.cs
+++ Cryptid/Pages/Create.razor.cs
@@ -8,6 +8,8 @@ namespace Cryptid.Pages
 
         private CreateState State { get; set; }
 
+        private Player Player { get; set; }
+
         public Create()
         {
             State = CreateState.SelectDifficulty;
@@ -15,8 +17,8 @@ namespace Cryptid.Pages
 
         private void SubmitDifficulty(Difficulty difficulty, Player player, List<Player> otherPlayers)
         {
+            Player = player;
             State = CreateState.SelectRule;
-            Console.Out.WriteLine(State);
             StateHasChanged();
         }
 
